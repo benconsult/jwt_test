4f41fb6e3c48ece1f221affa40b25b0a96892410
benconsult <bendayo38@gmail.com>
2024-03-22 02:34:55 +0100

    postman

--- starter/controllers/main.js
+++ starter/controllers/main.js
@@ -6,8 +6,7 @@ const login = async (req,res)=>{
 
 const dashboard = async(req,res)=>{
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello, Ben Bruce`, secret:`Here is
-    your lucky Number ${luckyNumber}`})
+    res.status(200).json({msg:`Hello, Ben Bruce`, secret:`Here is your lucky Number ${luckyNumber}`})
 }
 
 module.exports = {login, dashboard}
