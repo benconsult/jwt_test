2db4a15de932c9d7c4881c1e4c57ba07f788e577
benconsult <bendayo38@gmail.com>
2024-03-22 04:18:42 +0100

    send bearer token

--- starter/controllers/main.js
+++ starter/controllers/main.js
@@ -16,6 +16,8 @@ const login = async (req,res)=>{
 }
 
 const dashboard = async(req,res)=>{
+    //send bearer token 
+    console.log(req.headers);
     const luckyNumber = Math.floor(Math.random()*100)
     res.status(200).json({msg:`Hello, Ben Bruce`, secret:`Here is your lucky Number ${luckyNumber}`})
 }
