b52e461acec8e3adf243790a23eb5c296d084727
benconsult <bendayo38@gmail.com>
2024-03-22 04:29:16 +0100

    check auth header

--- starter/controllers/main.js
+++ starter/controllers/main.js
@@ -16,8 +16,13 @@ const login = async (req,res)=>{
 }
 
 const dashboard = async(req,res)=>{
-    //send bearer token 
-    console.log(req.headers);
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No token provided', 401)
+   
+    }
+    const token = authHeader.split(' ')[1]
+    console.log(token);
     const luckyNumber = Math.floor(Math.random()*100)
     res.status(200).json({msg:`Hello, Ben Bruce`, secret:`Here is your lucky Number ${luckyNumber}`})
 }
