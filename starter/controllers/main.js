71d647b79b6a538b509b9af57fe8391bfc67a251
benconsult <bendayo38@gmail.com>
2024-03-22 02:23:42 +0100

    controllers and routes setup

---
+++ starter/controllers/main.js
@@ -0,0 +1,13 @@
+
+
+const login = async (req,res)=>{
+    res.send('Fake login/Register/Signup Route')
+}
+
+const dashboard = async(req,res)=>{
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg:`Hello, Ben Bruce`, secret:`Here is
+    your lucky Number ${luckyNumber}`})
+}
+
+module.exports = {login, dashboard}
